9bc863d37d4642f8faf60b1ca6ff89736f7460c8
vitorsonn <provitor39@gmail.com>
2025-08-22 16:01:15 -0300

    closes #6 - Primeira lógica do botão somar

---
+++ operações.js
@@ -0,0 +1,17 @@
+// criar a função que vai somar os 2 valores dos inputs
+//receber o valor dos 2 inputs e armazenar em 2 variáveis
+// realizar a soma das 2 variáveis e colocar em outra variavel
+// mostrar no ultimo input o resultado
+
+
+function calc(){
+    let num1 = parseFloat(document.getElementById("number1").value) 
+    //parseFloat ou ParseInt são métodos que convertem string em numeros
+    let num2 = parseFloat(document.getElementById("number2").value)
+
+    let result = num1 +  num2
+
+    document.getElementById("result").value = result // o ultimo input recebe o valor do resultado
+
+    
+}
